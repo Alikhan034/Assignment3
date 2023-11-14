4fdacd858a8b43f5dc282c2047e365b003f0d00f
Muhammad Ali Khan <alikhan809212@gmail.com>
2023-11-14 23:18:11 +0500

    Java Calculator

---
+++ CalculatorTest.java
@@ -0,0 +1,19 @@
+// CalculatorTest.java
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        assertEquals(5, calculator.add(2, 3));
+        assertEquals(-1, calculator.add(-2, 1));
+    }
+
+    @Test
+    public void testSubtract() {
+        Calculator calculator = new Calculator();
+        assertEquals(1, calculator.subtract(3, 2));
+        assertEquals(-3, calculator.subtract(0, 3));
+    }
+}
